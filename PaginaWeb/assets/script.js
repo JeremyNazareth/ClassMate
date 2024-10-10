c67a8faaf63c5640f0cda6ce511cf9ec7bb70a40
JeremyReyesDuocUc <jer.reyesr@duocuc.cl>
2024-10-10 15:51:52 -0300

    avance de creacion de bloque con js

--- PaginaWeb/assets/script.js
+++ PaginaWeb/assets/script.js
@@ -2,6 +2,25 @@ const blocks = {}
 let idCount = 0;
 
 function createblock () {
-    cons
 
+    const name = document.getElementById('blockName').value;
+    const description = document.getElementById('blockDescription').value;
+
+    if (name){
+        const id = `blockId-${idCount++}`;
+        blocks[id] = {
+            id: id,
+            name: name,
+            description: description
+
+        };
+
+        document.getElementById('output').textContent = JSON.stringify
+        document.getElementById('nameBlock')
+
+    } else{
+        alert('Complete los campos.');
+    }
 }
+
+document.getElementById('createBlock').addEventListener('click', createBlock);
